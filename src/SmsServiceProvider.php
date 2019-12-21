90af6d0fba166d18c20fb6b004d7c47e0dc86bd5
clzola <lazar.radinovic@gmail.com>
2019-12-21 14:26:27 +0100

    Adds service provider

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace clzola\Components\Sms;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
