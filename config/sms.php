7d586baab4b20b62622302c0324062090b6a7814
clzola <lazar.radinovic@gmail.com>
2019-12-21 18:00:42 +0100

    Adds documentation blocks to config file

--- config/sms.php
+++ config/sms.php
@@ -8,6 +8,9 @@ return [
     'default' => env('SMS_DRIVER', 'null'),
 
 
+    /*
+     * Specify sender name.
+     */
     'from' => env('SMS_FROM', 'Laravel'),
 
 
@@ -15,8 +18,19 @@ return [
      * List of drivers and theirs configurations.
      */
     'drivers' => [
+
+        /*
+         * Driver for sending sms messages to running emulator.
+         */
         'emulator' => [
+
+            /*
+             * Specify Android SDK path
+             */
             'android_sdk_path' => env('SMS_ANDROID_SDK_PATH'),
+
         ]
+
     ]
+
 ];
